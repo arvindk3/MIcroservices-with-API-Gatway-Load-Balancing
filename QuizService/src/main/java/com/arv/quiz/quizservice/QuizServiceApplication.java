9d42347824f8d4b11b1eb40d16ac5b83b01ef933
Arvind Kumar <arvind.ks3012@gmail.com>
2024-02-25 20:07:06 +0530

    Implimented Curcuit breaker & Retry mechanism using resilience4j

--- QuizService/src/main/java/com/arv/quiz/quizservice/QuizServiceApplication.java
+++ QuizService/src/main/java/com/arv/quiz/quizservice/QuizServiceApplication.java
@@ -2,7 +2,9 @@ package com.arv.quiz.quizservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class QuizServiceApplication {
 
